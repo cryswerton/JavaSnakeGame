06c42b130547170c544f70ae801027aeb76a9969
cryswerton <cryswertons@gmail.com>
2021-12-04 12:16:17 -0300

    first version

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// First version Originally created by Bro Code channel
+// here's the link for the video: https://youtu.be/bI6e6qjJ8JQ
+
+public class Main {
+
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+
+}
